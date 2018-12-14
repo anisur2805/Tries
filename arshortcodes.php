2b3afd10532a57480be479c9a93b1f4173094caf
Anisur Rahman <anisur2805@users.noreply.github.com>
2018-12-14 08:35:26 +0600

    Update arshortcodes.php

--- arshortcodes.php
+++ arshortcodes.php
@@ -1,52 +1,39 @@
 <?php
 function artshortcode_shortcode_wrapper($atts, $content = null) {
-	// Attributes
-	$atts = shortcode_atts(
-		array(
-			'view'	=> 'carousel',
-		),
-		$atts,
-		'art-wrapper'
-	);
-	// Attributes in var
-	$view = $atts['carousel'];
+	ob_start();
 	?>
-	<div class="artestimonials">
-		<?php echo do_shortcode( $content ); ?>
-		
-		<?php
-		
-	}
-	add_shortcode( 'art-wrapper', 'artshortcode_shortcode_wrapper' );
-	function artshortcode_shortcode($atts, $content = null) {
-	// Attributes
-		$atts = shortcode_atts(
-			array(
-				'name' => 'afd',
-				'thumb' => 'http://localhost/wordpress/wp-content/uploads/2018/12/Screenshot_2.png',
-			),
-			$atts,
-			'arctestimonial'
-		);
-	// Attributes in var
-		$name = $atts['name'];
-		$thumb = $atts['thumb'];
-	// Your Code
-		?>
-		<div class="owl-carousel owl-theme arc-testimonial-wrapper">
-			<div class="art-thumb">
-				<img src="<?php echo $thumb; ?>" alt="">
-			</div>
-			<div class="art-content">
-				<div class="item">
-					<p><?php echo do_shortcode($content) ?></p>
-					<p><strong><?php echo $name; ?></strong></p>
-				</div>
-			</div>
+	<div class="parent">
+		<div class="artestimonials owl-theme arc-testimonial-wrapper owl-carousel">
+			<?php echo do_shortcode(wp_strip_all_tags($content)); ?>
 		</div>
 	</div>
-</div>
-<?php
-
+	<?php
+	$output = ob_get_clean();
+	return $output; 
 }
-add_shortcode( 'arctestimonial', 'artshortcode_shortcode' );
+add_shortcode( 'artestimonial_wrapper', 'artshortcode_shortcode_wrapper' );
+function artshortcode_shortcode($atts, $content = null) {
+	$atts = shortcode_atts(
+		array(
+			'name' => '',
+			'thumb' => '[home_url]/wp-content/uploads/2018/08/T1_-150x150.jpg',
+		),
+		$atts,
+		'artestimonial'
+	);
+	$name = $atts['name'];
+	$thumb = $atts['thumb']; ?>
+	<div class="item">
+		<div class="art-thumb">
+			<?php if(!empty($thumb)) { 
+				echo '<img src=" '. $thumb .' " alt="">';
+			} ?>
+		</div>
+		<div class="art-content">
+			<p class="arcontent"><?php echo wp_strip_all_tags($content); // escape all html tag ?></p>
+			<p class="arname"><strong><?php echo wp_strip_all_tags($name); ?></strong></p>
+		</div>
+	</div>
+	<?php
+}
+add_shortcode( 'artestimonial', 'artshortcode_shortcode' );
